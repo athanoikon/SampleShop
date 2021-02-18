89966d641e41b25726f39839e1ffd2db8f9d269c
Athanassia Oikonomou <athanassia.oikonomou@eurodyn.com>
2021-02-18 12:49:07 +0200

    Create JWT authentication

---
+++ SampleShop/SampleShop.ProductManagement.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SampleShop.ProductManagement.Models.DatabaseContext;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleShop.ProductManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        private List<User> appUsers = new List<User> {
+            new User {
+                FullName = "Nassia Oiko",
+                UserName = "admin",
+                Password = "1234",
+                UserRole = "Admin" },
+            new User {
+                FullName = "Test User",
+                UserName = "user",
+                Password = "1234",
+                UserRole = "User" }
+        };
+        public AuthenticationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] User login)
+        {
+            User user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJwtToken(user);
+                return Ok(new { token = tokenString, userDetails = user });
+            }
+
+            return Unauthorized();
+        }
+
+        private User AuthenticateUser(User credentials)
+        {
+            User user = appUsers.SingleOrDefault(x => x.UserName == credentials.UserName && x.Password == credentials.Password);
+            return user;
+        }
+
+        private string GenerateJwtToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim("fullName", userInfo.FullName.ToString()),
+                new Claim("role", userInfo.UserRole),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), };
+
+            var token = new JwtSecurityToken(issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- SampleShop/SampleShop.ProductManagement.Api/Startup.cs
+++ SampleShop/SampleShop.ProductManagement.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -9,7 +10,10 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using SampleShop.ProductManagement.Api.Extensions;
 
 namespace SampleShop.ProductManagement.Api
 {
@@ -26,6 +30,30 @@ namespace SampleShop.ProductManagement.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var key = Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"]);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddAuthorization(config=> {
+                config.AddPolicy(PoliciesExtensions.Admin, PoliciesExtensions.AdminPolicy());
+                config.AddPolicy(PoliciesExtensions.User, PoliciesExtensions.UserPolicy());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,9 +66,10 @@ namespace SampleShop.ProductManagement.Api
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseRouting();
-
             app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
